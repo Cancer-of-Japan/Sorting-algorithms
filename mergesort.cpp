21d6f31a7f8f5a928343097b12e9265398587ac4
Cancer-of-Japan <leonardotoshi@gmail.com>
2021-04-05 23:00:42 +0800

    bla bla

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,70 +1,50 @@
-#include <stdio.h>
-#include <math.h>
+#include <iostream>
 
 using namespace std;
 
-void printArray(int arr[], int n);
-void merge(int arr[], int l, int m, int r);
-void mergeSort(int arr[], int l, int r);
-
-int main(){
-    int arr[] = {5, 4, 3, 2, 1};
-    int n = sizeof(arr)/sizeof(arr[0]);
-	printf("Original...\n");
-	printArray(arr, n);
-	printf("Process Begin...\n");
-	mergeSort(arr, 0, n - 1);
-	printf("Result...\n");
-    printArray(arr, n);
-     
-     return 0;
-}
-
-void printArray(int arr[], int n){
-    int i;
-    for(i = 0; i < n; i++){
-        printf("%d ",arr[i]);
-    }
-    printf("\n");
-}
 
 void merge(int arr[], int l, int m, int r){
-    int n1 = m - 1 + 1;
+    int n1 = m - l + 1;
     int n2 = r - m;
 
     int L[n1], R[n2];
-    //Divide original array to 2 
+    
+	//Divide original array to 2 
     for(int i = 0; i < n1; i++){
         L[i] = arr[l + i];
     }
     for(int j = 0; j < n2; j++){
         R[j] = arr[m + 1 + j];
     }
+
     
     //i...initial index of 1st sub-arr
     int i = 0;
     //j...initial index of 2nd sub-arr
     int j = 0;
     //k...initial index of merged sub-arr
-    int k = 0;
+    int k = l;
 
     while(i < n1 && j < n2){
         if(L[i] <= R[j]){
             arr[k] = L[i];
             i++;
-        }else{
+        }
+		else{
             arr[k] = R[j];
             j++;
         }
         k++;
     }
-
+	
+	//Copy the remaining elements of L[], if there are any
     while(i < n1){
         arr[k] = L[i];
         i++;
         k++;
     }
-
+	
+	//Copy the remaining elements of R[], if there are any
     while(j < n2){
         arr[k] = R[j];
         j++;
@@ -82,6 +62,30 @@ void mergeSort(int arr[], int l, int r){
     merge(arr, l, m, r);
 }
 
+
+void printArray(int arr[], int n){
+    int i;
+    for(i = 0; i < n; i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+	printf("Original...\n");
+	printArray(arr, n);
+	
+	printf("Process Begin...\n");
+	mergeSort(arr, 0, n - 1);
+	cout << "Done" << endl;
+	printf("Result...\n");
+    printArray(arr, n);
+     
+     return 0;
+}
 
 
 
